b581968ee1b3f15a610e62735d6c3c3b31cdf331
Fernando Romani <hec27080377@gmail.com>
2020-04-27 23:11:23 -0500

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -13,6 +13,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) { 
 
@@ -24,20 +25,76 @@ export class ProductosService {
 
   private cargarProductos(){
 
-    
+    return new Promise( (resolve, reject) =>{
 
-    this.http.get('https://angular-html-82e62.firebaseio.com/productos_idx.json')
+
+      this.http.get('https://angular-html-82e62.firebaseio.com/productos_idx.json')
         .subscribe((resp: Producto[]) => {
 
 
-          console.log(resp);
+          //console.log(resp);
 
           this.productos = resp;
           this.cargando= false;
+          resolve();
 
           
 
         });
+
+    });
+
+    
+ 
   }
+
+
+  getProducto( id: string){
+
+    return this.http.get(`https://angular-html-82e62.firebaseio.com/productos/${ id }.json`);
+
+  }
+
+  buscarProducto( termino: string){
+
+    if ( this.productos.length === 0){
+      // cargar productos
+      this.cargarProductos().then(()=>{
+        // ejecutar despues de tener los productos
+        //aplicar filtro
+
+        this.filtrarProductos ( termino);
+      });
+    }else {
+      // aplicar el filtro
+      this.filtrarProductos ( termino);
+    }
+
+
+ 
+
+    //console.log(this.productosFiltrado);
+  }
+
+    private filtrarProductos (termino: string){
+
+      // console.log(this.productos);
+
+      this.productosFiltrado= [];
+
+      termino= termino.toLocaleLowerCase();
+
+      this.productos.forEach (prod => { 
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if ( prod.categoria.indexOf (termino) >=0 || tituloLower.indexOf (termino) >=0){
+          this.productosFiltrado.push( prod);
+        }
+
+
+
+      });
+    }
 
 }
