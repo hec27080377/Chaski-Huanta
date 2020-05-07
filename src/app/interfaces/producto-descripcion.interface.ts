8265afba990894d9ae27b6471a7b55259692e3c3
Fernando Romani <hec27080377@gmail.com>
2020-05-07 12:33:04 -0500

    Listo para desplegar2

--- src/app/interfaces/producto-descripcion.interface.ts
+++ src/app/interfaces/producto-descripcion.interface.ts
@@ -1,8 +1,7 @@
 export interface ProductoDescripcion {
-  categoria: string;
+
   desc1: string;
-  desc2: string;
-  producto: string;
+  desc2: string;  
   resumen: string;
   subtitulo1: string;
   subtitulo2: string;
